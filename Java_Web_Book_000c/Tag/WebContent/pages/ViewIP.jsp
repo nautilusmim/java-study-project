a4465aa54d730407a6b76f50ea9a21f83dabd8dd
nautilusmim <biologist1521@126.com>
2019-08-30 17:22:39 +0800

    Add JSP user defined tag project.

---
+++ Java_Web_Book_000c/Tag/WebContent/pages/ViewIP.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://www.it315.org/taglib" prefix="it315" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Test usage of JSP self-defined tag</title>
+</head>
+<body>
+	Local IP: <it315:viewIP />
+</body>
+</html>
---
+++ Java_Web_Book_000c/Tag/src/org/it315/custom/tag/ViewIPTag.java
@@ -0,0 +1,32 @@
+package org.it315.custom.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class ViewIPTag extends TagSupport {
+
+	private static final long serialVersionUID = -2736280132496855807L;
+	
+	@Override
+	public int doStartTag() throws JspException {
+		JspWriter out = pageContext.getOut();
+		String ip = pageContext.getRequest()
+								.getRemoteHost()
+								//.getRemoteAddr()
+								;
+		
+		try {
+			out.write(ip);
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return super.doStartTag();
+	}
+
+}
