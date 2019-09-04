06459fe9fdac8fa3cb2bb86f686786bb53c9fa4a
nautilusmim <biologist1521@126.com>
2019-09-04 14:57:58 +0800

    Recursively operate tag body.

---
+++ Java_Web_Book_000c/Tag/WebContent/pages/TestIterate.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://www.it315.org/taglib" prefix="it315" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Test user defined tag - tag body executed iteratively</title>
+</head>
+<body>
+	<%!
+		String[] items = {"Java", "C/C++", "Assembly", "Python", "Perl"};
+	%>
+	<it315:iterate items="<%= items %>" name="lang">
+		<%= pageContext.findAttribute("lang").toString() %> <br />
+		${ lang } <br />
+	</it315:iterate>
+</body>
+</html>
---
+++ Java_Web_Book_000c/Tag/src/org/it315/custom/tag/IterateTag.java
@@ -0,0 +1,52 @@
+package org.it315.custom.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class IterateTag extends TagSupport {
+
+	private static final long serialVersionUID = -3358086024347216429L;
+	
+	private String name;
+	
+	private String[] items;
+	
+	private int i = 0;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getItems() {
+		return items;
+	}
+
+	public void setItems(String[] items) {
+		this.items = items;
+	}
+	
+	@Override
+	public int doStartTag() throws JspException {
+		if(null == items || items.length <= 0) {
+			return SKIP_BODY;
+		} else {
+			pageContext.setAttribute(name, items[0]);
+			return EVAL_BODY_INCLUDE;
+		}
+	}
+	
+	@Override
+	public int doAfterBody() throws JspException {
+		if(++i < items.length) {
+			pageContext.setAttribute(name, items[i]);
+			return EVAL_BODY_AGAIN;
+		} else {
+			return SKIP_BODY;
+		}
+	}
+
+}
